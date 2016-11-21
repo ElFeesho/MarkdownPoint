9ac778788dc3a7b1fe4b02f282e683500c0fbd62
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-11-21 22:12:35 +0000

    Generalise string splitting

--- markdownpoint/test/markdownparse.cpp
+++ markdownpoint/test/markdownparse.cpp
@@ -13,6 +13,7 @@ public:
     MarkdownPoint::Presentation parse(const std::string &markdown);
 private:
     std::vector<std::string> splitSlides(const std::string &rawInput);
+    std::vector<std::string> split(const std::string &rawInput, const std::string &delimeter);
 };
 
 MarkdownPoint::Presentation MarkdownPresentationParser::parse(const std::string &markdown)
@@ -28,20 +29,24 @@ MarkdownPoint::Presentation MarkdownPresentationParser::parse(const std::string 
 }
 
 std::vector<std::string> MarkdownPresentationParser::splitSlides(const std::string &rawInput) {
+    return split(rawInput, "<==>");
+}
+
+std::vector<std::string> MarkdownPresentationParser::split(const std::string &rawInput, const std::string &delimeter)
+{
     std::vector<std::string> individualSlides;
 
     std::string inputLeft = rawInput;
-    while(inputLeft.find("<==>") != std::string::npos)
+    while(inputLeft.find(delimeter) != std::string::npos)
     {
-        individualSlides.push_back(std::string(inputLeft, 0, inputLeft.find("<==>")));
-        inputLeft = inputLeft.substr(inputLeft.find("<==>")+4);
+        individualSlides.push_back(std::string(inputLeft, 0, inputLeft.find(delimeter)));
+        inputLeft = inputLeft.substr(inputLeft.find(delimeter)+delimeter.size());
     }
 
     individualSlides.push_back(inputLeft);
 
     return individualSlides;
 }
-
 
 TEST(markdown_parsing, can_parse_an_empty_page) {
     MarkdownPresentationParser p;
