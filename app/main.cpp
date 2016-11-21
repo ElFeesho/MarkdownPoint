71223cc116be23920beae04501d32655eae93d2f
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-11-21 18:07:31 +0000

    Initial commit

---
+++ app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <markdownpoint.hpp>
+
+int main(int argc, char **argv)
+{
+	MarkdownPoint::Presentation presentation;
+	return 0;
+}
---
+++ markdownpoint/include/markdownpoint.hpp
@@ -0,0 +1,11 @@
+#ifndef __MARKDOWN_POINT_HPP__
+#define __MARKDOWN_POINT_HPP__
+
+namespace MarkdownPoint {
+	class Presentation {
+	public:
+		Presentation();
+	};
+}
+
+#endif
