49a0e816426fb4b68cb9355044c815df9e0bd498
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-11-21 22:08:34 +0000

    Add ability to parse multiple slides

--- markdownpoint/test/markdownparse.cpp
+++ markdownpoint/test/markdownparse.cpp
@@ -2,6 +2,8 @@
 // Created by Christopher Sawczuk on 21/11/2016.
 //
 
+#include <string>
+
 #include <gtest/gtest.h>
 #include <markdownpoint.hpp>
 
@@ -9,15 +11,35 @@ class MarkdownPresentationParser
 {
 public:
     MarkdownPoint::Presentation parse(const std::string &markdown);
+private:
+    std::vector<std::string> splitSlides(const std::string &rawInput);
 };
 
 MarkdownPoint::Presentation MarkdownPresentationParser::parse(const std::string &markdown)
 {
     MarkdownPoint::Presentation presentation;
 
-    MarkdownPoint::Slide *slide = presentation.addSlide();
+    for(std::string slideString : splitSlides(markdown))
+    {
+        presentation.addSlide();
+    }
 
     return presentation;
+}
+
+std::vector<std::string> MarkdownPresentationParser::splitSlides(const std::string &rawInput) {
+    std::vector<std::string> individualSlides;
+
+    std::string inputLeft = rawInput;
+    while(inputLeft.find("<==>") != std::string::npos)
+    {
+        individualSlides.push_back(std::string(inputLeft, 0, inputLeft.find("<==>")));
+        inputLeft = inputLeft.substr(inputLeft.find("<==>")+4);
+    }
+
+    individualSlides.push_back(inputLeft);
+
+    return individualSlides;
 }
 
 
@@ -26,4 +48,11 @@ TEST(markdown_parsing, can_parse_an_empty_page) {
     MarkdownPoint::Presentation presentation = p.parse("");
 
     EXPECT_EQ(presentation.slideCount(), 1);
+}
+
+TEST(markdown_parsing, can_parse_multiple_slides) {
+    MarkdownPresentationParser p;
+    MarkdownPoint::Presentation presentation = p.parse("<==><==>");
+
+    EXPECT_EQ(presentation.slideCount(), 3);
 }
