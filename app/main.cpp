ee208ef757410a059e260bd613926d1e91e16d3c
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-11-22 22:00:23 +0000

    Create HPDF based presentation renderer

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
 #include <markdownpoint.hpp>
+#include <hpdf.h>
 
-int main(int argc, char **argv)
-{
-	MarkdownPoint::Presentation presentation;
-	return 0;
+class HPdfPresentationRenderer : public MarkdownPoint::Renderer {
+public:
+    HPdfPresentationRenderer() {
+        pdf = HPDF_New([](HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data) {
+            std::cerr << "Error (" << error_no << ", " << detail_no << ")" << std::endl;
+        }, nullptr);
+
+        helvetica = HPDF_GetFont(pdf, "Helvetica", NULL);
+        courier = HPDF_GetFont(pdf, "Courier", NULL);
+    }
+
+    ~HPdfPresentationRenderer() override {
+        HPDF_Free(pdf);
+    }
+
+    void renderPage(MarkdownPoint::Slide *slide) override {
+        addNewPage();
+    }
+
+    void renderHeading(MarkdownPoint::Heading *heading) override {
+        HPDF_Page_SetFontAndSize(currentPage, helvetica, sizes[heading->size()-1]);
+        float tw = HPDF_Page_TextWidth(currentPage, heading->text().c_str());
+        HPDF_Page_SetRGBFill(currentPage, 0.8, 0.8, 0.8);
+        HPDF_Page_BeginText(currentPage);
+        HPDF_Page_TextOut(currentPage, (HPDF_Page_GetWidth(currentPage) - tw) / 2, HPDF_Page_GetHeight(currentPage) - 100, heading->text().c_str());
+        HPDF_Page_EndText(currentPage);
+    }
+
+    void writeToFile(const std::string &filename) {
+        HPDF_SaveToFile(pdf, filename.c_str());
+    }
+
+private:
+    void addNewPage() {
+        currentPage = HPDF_AddPage(pdf);
+        HPDF_Page_SetSize(currentPage, HPDF_PAGE_SIZE_A4, HPDF_PAGE_LANDSCAPE);
+        HPDF_Page_SetRGBFill(currentPage, 0.3, 0.3, 0.3);
+        HPDF_Page_Rectangle(currentPage, 0, 0, HPDF_Page_GetWidth(currentPage), HPDF_Page_GetHeight(currentPage));
+        HPDF_Page_Fill(currentPage);
+    }
+
+    HPDF_Doc pdf;
+    HPDF_Page currentPage;
+    HPDF_Font helvetica;
+    HPDF_Font courier;
+    int sizes[6] { 48, 36, 24, 18, 14, 12 };
+};
+
+int main(int argc, char **argv) {
+    MarkdownPoint::MarkdownPresentationParser parser;
+    MarkdownPoint::Presentation presentation = parser.parse(
+            "# This is a test<==># Of the markdown<==># Presentation maker<==># MarkdownPoint");
+    HPdfPresentationRenderer renderer;
+    MarkdownPoint::PresentationRenderer presentationRenderer(&renderer);
+    presentationRenderer.render(presentation);
+
+    renderer.writeToFile("test.pdf");
+    return 0;
 }
