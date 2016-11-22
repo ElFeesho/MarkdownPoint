1676aae56ff4fd4712a09621868e058c0d7e325d
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-11-22 21:31:59 +0000

    Add test for presentation rendering

--- markdownpoint/test/presentationrenderer.cpp
+++ markdownpoint/test/presentationrenderer.cpp
@@ -12,6 +12,7 @@ namespace MarkdownPoint {
     public:
         virtual ~Renderer(){}
         virtual void renderPage(Slide *slide) = 0;
+        virtual void renderHeading(Heading *heading) = 0;
     };
 
     class PresentationRenderer {
@@ -27,7 +28,15 @@ namespace MarkdownPoint {
     }
 
     void PresentationRenderer::render(Presentation &p) {
-        _renderer->renderPage(p.slide(0));
+        for (uint32_t i = 0; i < p.slideCount(); i++)
+        {
+            Slide *slide = p.slide(i);
+            _renderer->renderPage(slide);
+            for (uint32_t k = 0; k < slide->blockCount(); k++)
+            {
+                _renderer->renderHeading(dynamic_cast<Heading *>(slide->block(k)));
+            }
+        }
     }
 }
 
@@ -42,19 +51,38 @@ public:
         renderPageCalled = true;
     }
 
+    void renderHeading(MarkdownPoint::Heading *heading) {
+        renderHeadingCalled = true;
+    }
+
     bool renderPageCalled;
+    bool renderHeadingCalled;
 };
 
 TEST(presentation_renderer, will_delegate_creation_of_slide) {
     SpyRenderer *spyRenderer = new SpyRenderer();
 
     MarkdownPoint::PresentationRenderer renderer(spyRenderer);
-    
+
     MarkdownPoint::Presentation presentation;
     presentation.addSlide();
 
     renderer.render(presentation);
 
     EXPECT_EQ(spyRenderer->renderPageCalled, true);
+}
+
+TEST(presentation_renderer, will_render_a_header) {
+    SpyRenderer *spyRenderer = new SpyRenderer();
+
+    MarkdownPoint::PresentationRenderer renderer(spyRenderer);
+
+    MarkdownPoint::Presentation presentation;
+    MarkdownPoint::Slide *slide = presentation.addSlide();
+    slide->addBlock(new MarkdownPoint::Heading("Expected Header"));
+
+    renderer.render(presentation);
+
+    EXPECT_EQ(spyRenderer->renderHeadingCalled, true);
 }
 
