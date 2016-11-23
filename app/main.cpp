c80bc511a85c5c4a4c0a851f6ce871c48d00273b
Chris Sawczuk <chris.sawczuk@bbc.co.uk>
2016-11-23 08:23:08 +0000

    Add paragraph rendering
    Difficult to understand how to proceed with text wrapping. The API allows
    you to determine how much text was rendered before it needed to wrap.

--- app/main.cpp
+++ app/main.cpp
@@ -31,13 +31,24 @@ public:
     }
 
     void renderParagraph(MarkdownPoint::Paragraph *paragraph) override {
+        const char *text = paragraph->text().c_str();
+        std::cout << "Text length: " << paragraph->text().size() << std::endl;
+        HPDF_REAL width = 0;
+        HPDF_UINT outwidth = HPDF_Font_MeasureText(helvetica, (const HPDF_BYTE *) text, (HPDF_UINT) paragraph->text().size(), textBoundaryWidth, 16, 0, 0, true, &width);
         HPDF_Page_SetFontAndSize(currentPage, helvetica, 16);
+        std::cout << "Out width: " << outwidth << std::endl;
+        std::cout << "width: " << width << std::endl;
+
         HPDF_UINT len = 0;
         HPDF_Page_SetRGBFill(currentPage, 0.8, 0.8, 0.8);
         HPDF_Page_BeginText(currentPage);
-        HPDF_Page_TextRect(currentPage, 100, HPDF_Page_GetHeight(currentPage) - 125, HPDF_Page_GetWidth(currentPage), 100, paragraph->text().c_str(), HPDF_TALIGN_LEFT, &len);
-        std::cout << "Text length: " << len << std::endl;
+        HPDF_Page_TextRect(currentPage, 100, HPDF_Page_GetHeight(currentPage) - textYPosition, textBoundaryOffset+textBoundaryWidth, 100, text, HPDF_TALIGN_LEFT, &len);
+        if (outwidth != paragraph->text().size())
+        {
+            textYPosition += 18;
+        }
         HPDF_Page_EndText(currentPage);
+        textYPosition += 18;
     }
 
     void writeToFile(const std::string &filename) {
@@ -51,19 +62,26 @@ private:
         HPDF_Page_SetRGBFill(currentPage, 0.3, 0.3, 0.3);
         HPDF_Page_Rectangle(currentPage, 0, 0, HPDF_Page_GetWidth(currentPage), HPDF_Page_GetHeight(currentPage));
         HPDF_Page_Fill(currentPage);
+        textBoundaryWidth = HPDF_Page_GetWidth(currentPage)-textBoundaryOffset*2;
+        textYPosition = 125;
     }
 
     HPDF_Doc pdf;
     HPDF_Page currentPage;
     HPDF_Font helvetica;
     HPDF_Font courier;
+
+    HPDF_REAL textBoundaryWidth;
+    HPDF_REAL textBoundaryOffset { 100 };
+    HPDF_REAL textYPosition { 125 };
+
     int sizes[4] { 48, 36, 24, 18 };
 };
 
 int main(int argc, char **argv) {
     MarkdownPoint::MarkdownPresentationParser parser;
     MarkdownPoint::Presentation presentation = parser.parse(
-            "# This is a test\nWoop<==># Of the markdown<==># Presentation maker<==># MarkdownPoint");
+            "# Introduction\nMarkdown is a useful tool and is understood by anyone who has spent considerable time on GitHub.\nIt can be read as plain text, as well as formatted by a viewer.");
     HPdfPresentationRenderer renderer;
     MarkdownPoint::PresentationRenderer presentationRenderer(&renderer);
     presentationRenderer.render(presentation);
